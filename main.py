ffed74de2bb03f4a5a3b352cd9fef39d275b6dfa
Petr Glumskov <petr171083@gmail.com>
2024-06-26 23:17:23 +0300

    add function mask_account_card

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from src import masks
+from src import widget
 
 if __name__ == '__main__':
-    # Проверка модуля masks
-    # print(masks.get_mask_card_number(7000792289606361))
-    # print(masks.get_mask_account(73654108430135874305))
+    # Проверка модуля widget
+    # print(widget.mask_account_card('Maestro 1596837868705199'))
+    # print(widget.mask_account_card('Счет 64686473678894779589'))
+    # print(widget.mask_account_card('MasterCard 7158300734726758'))
+    # print(widget.mask_account_card('Счет 35383033474447895560'))
+    # print(widget.mask_account_card('Visa Classic 6831982476737658'))
+    # print(widget.mask_account_card('Visa Platinum 8990922113665229'))
+    # print(widget.mask_account_card('Visa Gold 5999414228426353'))
+    # print(widget.mask_account_card('Счет 73654108430135874305'))
+
+    # print(widget.get_data('2018-07-11T02:26:18.671407'))
     pass
+
--- src/widget.py
+++ src/widget.py
@@ -1,26 +1,26 @@
-# from masks import get_mask_account, get_mask_card_number
-import masks
 from datetime import datetime
 
-def mask_account_card(account: str) -> str:
+from src import masks
+
+
+def mask_account_card(account_card: str) -> str:
     """Функция общей маскировки карты и счета"""
-    # get_mask_account()
-    # get_mask_card_number()
-    # Примеры входных данных:
-    #
-    # Maestro 1596837868705199
-    # Счет 64686473678894779589
-    # MasterCard 7158300734726758
-    # Счет 35383033474447895560
-    # Visa Classic 6831982476737658
-    # Visa Platinum 8990922113665229
-    # Visa Gold 5999414228426353
-    # Счет 73654108430135874305
-    masks.
-    pass
+    account_card_list = account_card.split()
+
+    # номер счёта 20-ти значный
+    # номер карты 16-ти значный
+    if len(account_card_list[-1]) == 20:
+        mask = masks.get_mask_account(int(account_card_list[-1]))
+    elif len(account_card_list[-1]) == 16:
+        mask = masks.get_mask_card_number(int(account_card_list[-1]))
+    else:
+        return ""
+
+    account_card_list[-1] = mask
+    return " ".join(account_card_list)
 
 
 def get_data(day_str: str) -> str:
     """Функция преобразования даты. Входной аргумент: 2018-07-11T02:26:18.671407. Выход функции: 11.07.2018"""
-    dt = datetime.strptime('2018-07-11T02:26:18.671407', '%Y-%m-%dT%H:%M:%S.%f')
-    return dt.strftime('%d.%m.%Y')
+    dt = datetime.strptime("2018-07-11T02:26:18.671407", "%Y-%m-%dT%H:%M:%S.%f")
+    return dt.strftime("%d.%m.%Y")
